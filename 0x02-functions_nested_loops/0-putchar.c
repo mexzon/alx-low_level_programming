fd3e8e917190504fd741b380691aa4afba4cde3c
mexzon <mexzon@gmail.com>
2023-07-18 06:54:08 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
- * Return:0
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-char c[9] = "_putchar";
-int i = 0;
-while (i < 8)
-{
-_putchar(c[i]);
-i++;
-}
-_putchar('\n');
+
+printf("_putchar\n");
+
 return (0);
 }
+
